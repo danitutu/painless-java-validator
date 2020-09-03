3f0efc03eeec9f0231b52a5b0f6b86e3740919dc
danitutu <valentindanieltutuianu@gmail.com>
2020-09-03 17:07:46 +0300

    adding violation provider utils to map each element and return a list of ViolationProvider

---
+++ src/main/java/com/github/danitutu/painlessjavavalidator/ViolationProviderUtils.java
@@ -0,0 +1,38 @@
+package com.github.danitutu.painlessjavavalidator;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+
+public class ViolationProviderUtils {
+    private ViolationProviderUtils() {
+    }
+
+    /**
+     * Creates {@link ViolationProvider}s from collection using a mapping function.
+     * The mapping function receives a pair of <code>(i, o)</code> where <code>i</code>
+     * is the index of current iterated element and <code>o</code> is the iterated
+     * element. The function must return a {@link ViolationProvider}.
+     *
+     * @param list   collection to be mapped
+     * @param mapper mapping function
+     * @param <T>    type of elements from collection
+     * @return list of {@link ViolationProvider}
+     */
+    public static <T> List<ViolationProvider> from(List<T> list,
+                                                   BiFunction<Integer, T, ViolationProvider> mapper) {
+        if (mapper == null) {
+            throw new IllegalArgumentException("mapper is required");
+        }
+        if (list == null) {
+            return emptyList();
+        }
+        return IntStream.range(0, list.size())
+                .mapToObj(i -> mapper.apply(i, list.get(i)))
+                .collect(toList());
+    }
+
+}
---
+++ src/test/java/com/github/danitutu/painlessjavavalidator/ViolationProviderTest.java
@@ -0,0 +1,62 @@
+package com.github.danitutu.painlessjavavalidator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static com.github.danitutu.painlessjavavalidator.ValidationRule.notBlank;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ViolationProviderUtilsTest {
+
+    @Test
+    @DisplayName("WHEN input is null THEN empty list")
+    void from1() {
+        List<ViolationProvider> list = ViolationProviderUtils.from(null, (i, o) -> null);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    @DisplayName("WHEN mapper is null THEN empty list")
+    void from2() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> ViolationProviderUtils.from(emptyList(), null));
+
+        assertEquals("mapper is required", ex.getMessage());
+    }
+
+    @Test
+    @DisplayName("WHEN collection is empty THEN empty list")
+    void from3() {
+        List<ViolationProvider> list = ViolationProviderUtils.from(emptyList(), (i, o) -> null);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    @DisplayName("WHEN input valid THEN for each element we must get a violation provider")
+    void from4() {
+        List<String> list = new LinkedList<>();
+        list.add("   ");
+        list.add("s");
+        list.add("b");
+
+        List<ViolationProvider> result = ViolationProviderUtils.from(
+                list,
+                (i, s) -> notBlank(String.format("input[%d]", i), s));
+
+        assertEquals(3, result.size());
+
+        long violations = result.stream()
+                .map(Supplier::get)
+                .filter(Optional::isPresent)
+                .count();
+
+        assertEquals(1, violations);
+    }
+}
